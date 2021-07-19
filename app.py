1fa85c4ecad4fd480b890ef187fb97542a8e35a7
minhnhatnguyen2003 <47096001+minhnhatnguyen2003@users.noreply.github.com>
2021-07-19 18:30:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
 
-file = open('taylor_swift_lyrics.csv')
-data = pd.read_csv(file,sep=";", encoding='cp1252')  
+data = pd.read_csv(taylor_swift_lyrics.csv,sep=";", encoding='cp1252')  
 df = pd.DataFrame(data)
 
 option=st.selectbox('What album do you want to create a Word Cloud for ?',
